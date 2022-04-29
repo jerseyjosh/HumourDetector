5e5e83ef6e24ffa0f299cc4f3729190b0d2b2b55
Josh Griffiths <josh@hakuna.co.uk>
2022-04-29 11:56:14 +0100

    now replies to parents of mentions

--- bot.py
+++ bot.py
@@ -1,8 +1,14 @@
 import tweepy
+import logging
 import pickle
 import time
 import numpy as np
 from secrets import API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET, TWITTER_ID
+
+logger = logging.getLogger("Tweepy")
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename="tweepy.log")
+logger.addHandler(handler)
 
 def getAPI(api_key, api_secret, access_token, access_token_secret):
     auth = tweepy.OAuth1UserHandler(api_key, api_secret, access_token, access_token_secret)
@@ -17,19 +23,16 @@ def classifyTweet(tweet: list[str]):
     proba = np.max(clf.predict_proba(tweet))
     return prediction, proba
 
-
 def generateResponse(funny, proba):
     if funny:
         return f'beep boop... I am {proba*100:.2f}% sure this is funny.'
     else:
         return f'beep boop... I am {proba*100:.2f}% sure this is not funny.'
 
-
 def extractText(tweet):
     tweet = tweet.full_text
-    tweet = tweet.replace('@HumourDetector', '')
-    return tweet
-
+    tweet = tweet.split('@HumourDetector', 1)
+    return tweet[-1]
 
 def readLastSeen(filename):
     with open(filename, 'r') as f:
@@ -47,9 +50,12 @@ def getLastReceived(messages):
             received.append(message)
     return received
 
+def getParent(tweet):
+    parentID = tweet.in_reply_to_status_id
+    parentTweet = api.get_status(id=parentID, tweet_mode='extended')
+    return parentTweet
 
 api = getAPI(API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-
 
 def reply():
     mentions = api.mentions_timeline(count=10, since_id=readLastSeen('tweetHistory.txt'), tweet_mode='extended')
@@ -57,13 +63,27 @@ def reply():
     receivedMessages = getLastReceived(messages)
     for tweet in mentions:
         clfInput = extractText(tweet)
-        clfOutput, clfProba = classifyTweet(clfInput)
-        clfResponse = generateResponse(clfOutput, clfProba)
-        api.update_status("@" + tweet.user.screen_name + " " + clfResponse, in_reply_to_status_id=tweet.id)
-        api.create_favorite(tweet.id)
-        api.create_friendship(user_id=tweet.user.id)
-        storeLastSeen('tweetHistory.txt', tweet.id)
-        print(f"stored {tweet.id}")
+        if clfInput:
+            clfOutput, clfProba = classifyTweet(clfInput)
+            clfResponse = generateResponse(clfOutput, clfProba)
+            api.update_status("@" + tweet.user.screen_name + " " + clfResponse, in_reply_to_status_id=tweet.id)
+            api.create_favorite(tweet.id)
+            api.create_friendship(user_id=tweet.user.id)
+            storeLastSeen('tweetHistory.txt', tweet.id)
+            print(f"stored {tweet.id}")
+        else:
+            parent = getParent(tweet)
+            clfInput = extractText(parent)
+            print(clfInput)
+            clfOutput, clfProba = classifyTweet(clfInput)
+            clfResponse = generateResponse(clfOutput, clfProba)
+            api.update_status("@" + parent.user.screen_name + " @" + tweet.user.screen_name + " " + clfResponse, in_reply_to_status_id=parent.id)
+            api.create_favorite(parent.id)
+            api.create_favorite(tweet.id)
+            api.create_friendship(user_id=parent.user.id)
+            api.create_friendship(user_id=tweet.user.id)
+            storeLastSeen('tweetHistory.txt', tweet.id)
+            print(f"stored {tweet.id}")
     if receivedMessages:
         for message in receivedMessages:
             messageID = int(message.id)
@@ -78,7 +98,3 @@ def reply():
 while True:
     reply()
     time.sleep(60)
-
-
-
-
